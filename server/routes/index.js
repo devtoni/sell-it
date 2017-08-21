99d85d85b6971c55f418adba753e6597d4a24dc7
devtoni <devtonirm@gmail.com>
2017-08-21 21:49:51 +0200

    video working

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
+router.get('/productos', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'server/views/productos.html'))
+})
+
 router.get('/', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'server/views/index.html'))
 })
