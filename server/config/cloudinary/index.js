c8834213cd6e33b9c87d7fa71790132d3a917bff
devtoni <devtonirm@gmail.com>
2017-09-03 13:06:01 +0200

    modified folder for multer

--- server/config/cloudinary/index.js
+++ server/config/cloudinary/index.js
@@ -1,7 +1,7 @@
 const cloudinary = require('cloudinary')
 const del = require('del')
 const path = require('path')
-const UPLOAD_FOLDER = process.env.UPLOAD_FOLDER
+const uploadFolderPath = path.join(__base, process.env.UPLOAD_FOLDER)
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -14,8 +14,7 @@ function uploadCloudinary (req, res, next) {
     cloudinary.uploader.upload(req.file.path, ({ url }) => {
       if (url) {
         req.body.imgUrl = url
-        // delete files inside folder but not the folder itself
-      //  del.sync([`${}/**`, `!${}`])
+        del.sync([`${uploadFolderPath}/**`, `!${uploadFolderPath}`])
         next()
       } else {
         res.status(404).send('Oh uh, something went wrong')
