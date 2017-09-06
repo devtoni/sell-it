aca34bc325d2aea4f1a5707c314b4fa538ca952e
devtoni <devtonirm@gmail.com>
2017-09-06 07:50:00 +0200

    solved issue when login post

--- server/routes/auth/handlers/handleLogin.js
+++ server/routes/auth/handlers/handleLogin.js
@@ -3,8 +3,9 @@ function postLogin (req, res) {
   const { user } = req
   if (user.gender) {
     res.redirect(`/profile/${user._id}`)
+  } else {
+    res.redirect('/edit-profile')
   }
-  res.redirect('/edit-profile')
 }
 
 module.exports = postLogin
